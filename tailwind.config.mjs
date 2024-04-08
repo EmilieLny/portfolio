05447860c19d3d108736edfa7487dd5aa9aa4143
Emilie Layani <emilielayani@gmail.com>
2024-04-08 11:06:16 -0400

    images interactive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			// backgroundImage: {
-			// 	'default': "url('essential-oil.png')"
-			// }
-		}
+		extend: {}
 	},
 	plugins: [],
 }
