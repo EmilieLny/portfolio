4e089d2dc49964d60c0581daf028ea3f06be396c
Emilie Layani <emilielayani@gmail.com>
2024-04-09 17:47:46 -0400

    design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {
+			keyframes: {
+				wave: {
+					'0%': { transform: 'rotate(0.0deg)' },
+					'10%': { transform: 'rotate(14.0deg)' },
+					'20%': { transform: 'rotate(-8.0deg)' },
+					'30%': { transform: 'rotate(14.0deg)' },
+					'40%': { transform: 'rotate(-4.0deg)' },
+					'45%': { transform: 'rotate(10.0deg)' },
+					'60%': { transform: 'rotate(0.0deg)' },
+					'100%': { transform: 'rotate(0.0deg)' },
+				}
+			},
+			animation: {
+				wave: 'wave 2s infinite'
+			}
+		}
 	},
 	plugins: [],
 }
